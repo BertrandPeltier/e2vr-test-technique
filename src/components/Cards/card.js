43ed11cfd76ff720cb16632233594ba2ee5d8d6e
BertrandPeltier <bertrandpeltier1979@gmail.com>
2021-09-03 13:52:56 +0200

    Modal trigger on Card

--- src/components/Cards/card.js
+++ src/components/Cards/card.js
@@ -1,42 +1,88 @@
 import React from 'react';
-import { Card, Image } from 'semantic-ui-react';
+import { Button, Card, Header, Image, Modal } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import CardModal from 'src/components/CardModal';
 
 // == Import
 import './styles.scss';
 
-const GameData = ({ game }) => {
-  const { background_image, name, released, rating, genres } = game;
+const GameCard = ({ game }) => {
+  const [open, setOpen] = React.useState(false);
+  const {
+    background_image, name, released, rating, ratings, genres, tags,
+  } = game;
   return (
-    <Card>
-      <Image src={background_image} wrapped ui={false} />
-      <Card.Content>
-        <Card.Header>{name}</Card.Header>
-        <Card.Meta><span className="subtitle">Release date :</span> {released}</Card.Meta>
-        <Card.Description>
-          <span className="subtitle">Rating :</span> {rating}<br />
+    <Modal
+      onClose={() => setOpen(false)}
+      onOpen={() => setOpen(true)}
+      open={open}
+      trigger={(
+        <Card>
+          <Image src={background_image} wrapped ui={false} />
+          <Card.Content>
+            <Card.Header>{name}</Card.Header>
+            <Card.Meta><span className="subtitle">Release date :</span> {released}</Card.Meta>
+            <Card.Description>
+              <span className="subtitle">Rating :</span> {rating}<br />
+              <ul className="liste">
+                <li><span className="subtitle">Genres :</span></li>
+                {genres.map((genre) => (
+                  <li key={genre.id}>{genre.name}</li>
+                ))}
+              </ul>
+            </Card.Description>
+          </Card.Content>
+        </Card>
+      )}
+    >
+      <Modal.Header>{name}</Modal.Header>
+      <Modal.Content image>
+        <Image className="modal__image" src={background_image} wrapped />
+        <Modal.Description className="modal__description">
+          <Header>Details</Header>
+          <p><span className="subtitle">Release date :</span> {released}</p>
+          <p><span className="subtitle">Rating :</span> {rating}</p>
+          <ul>
+            <li><span className="subtitle">Ratings :</span></li>
+            {ratings.map((ratingItem) => (
+              <li key={ratingItem.id}>{ratingItem.title} : {ratingItem.percent}%</li>
+            ))}
+          </ul>
+          <br />
           <ul className="liste">
             <li><span className="subtitle">Genres :</span></li>
             {genres.map((genre) => (
               <li key={genre.id}>{genre.name}</li>
             ))}
           </ul>
-        </Card.Description>
-      </Card.Content>
-      <CardModal data={game} />
-    </Card>
-  )
+          <br />
+          <ul className="liste">
+            <li><span className="subtitle">Tags :</span></li>
+            {tags.map((tag) => (
+              <li key={tag.id}>{tag.name}</li>
+            ))}
+          </ul>
+          <br />
+        </Modal.Description>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button color='black' onClick={() => setOpen(false)}>
+          Close
+        </Button>
+      </Modal.Actions>
+    </Modal>
+  );
 };
 
-GameData.propTypes = {
+GameCard.propTypes = {
   game: PropTypes.shape({
     name: PropTypes.string.isRequired,
     background_image: PropTypes.string.isRequired,
     released: PropTypes.string.isRequired,
     rating: PropTypes.number.isRequired,
+    ratings: PropTypes.array.isRequired,
     genres: PropTypes.array.isRequired,
+    tags: PropTypes.array.isRequired,
   }).isRequired,
 };
 
-export default GameData;
+export default GameCard;
--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Card } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import GameData from './card';
+import GameCard from './card';
 
 function Cards({ games }) {
   return (
     <Card.Group itemsPerRow={4} stackable doubling>
       {games.map((game) => (
-        <GameData key={game.id} game={game} />
+        <GameCard key={game.id} game={game} />
       ))}
     </Card.Group>
   );
